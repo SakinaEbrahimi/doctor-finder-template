4237db86f333a646c7464d563845f35f087065dc
Sakina Ebrahimi <sakinaebrahimi9@gmail.com>
2025-11-17 01:17:23 -0800

    Doctor finder template

---
+++ public/script/script.js
@@ -0,0 +1,40 @@
+const theme = document.getElementById('theme')
+const lightSv = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+<path d="M10 2a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 2ZM10 15a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 15ZM10 7a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM15.657 5.404a.75.75 0 1 0-1.06-1.06l-1.061 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM6.464 14.596a.75.75 0 1 0-1.06-1.06l-1.06 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM18 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 18 10ZM5 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 5 10ZM14.596 15.657a.75.75 0 0 0 1.06-1.06l-1.06-1.061a.75.75 0 1 0-1.06 1.06l1.06 1.06ZM5.404 6.464a.75.75 0 0 0 1.06-1.06l-1.06-1.06a.75.75 0 1 0-1.061 1.06l1.06 1.06Z" />
+</svg>
+`
+const darkSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+<path fill-rule="evenodd" d="M7.455 2.004a.75.75 0 0 1 .26.77 7 7 0 0 0 9.958 7.967.75.75 0 0 1 1.067.853A8.5 8.5 0 1 1 6.647 1.921a.75.75 0 0 1 .808.083Z" clip-rule="evenodd" />
+</svg>
+`
+theme.addEventListener('click', ()=>{
+    document.documentElement.classList.toggle('dark')
+    if(svgTheme()){
+        theme.innerHTML = lightSv
+    }else{
+        theme.innerHTML = darkSvg
+    }
+})
+
+function svgTheme(){ 
+    return document.documentElement.classList.contains('dark')
+}
+
+const close = `<svg xmlns="http://www.w3.org/2000/svg"
+ viewBox="0 0 24 24" fill="currentColor"><path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z">
+ </path></svg>`
+const menu = `
+<svg id="navburger" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path></svg>`
+const navburger  = document.getElementById('navburger')
+const navmenu = document.getElementById('navmenu')
+const navlink = document.querySelectorAll('nav_link')
+
+navburger.addEventListener('click',()=>{
+    navmenu.classList.toggle('right-[0]')
+    if(navmenu.classList.toggle('right-[0')){
+        navburger.innerHTML = close
+    }
+    else{
+        navburger.innerHTML = menu
+    }
+})
